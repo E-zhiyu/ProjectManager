6dd29b839091d3a718a5050556340415f1c8408f
E-zhiyu <E82487318@outlook.com>
2025-10-04 08:41:24 +0800

    构建基本框架

---
+++ app/src/main/java/com/project/manager/ui/dashboard/MineViewModel.java
@@ -0,0 +1,19 @@
+package com.project.manager.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MineViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public MineViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
