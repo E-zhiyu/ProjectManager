25b0fc6117bbed4766dec7b540745fc159635199
E_zhiyu <E82487318@outlook.com>
2025-11-25 20:19:02 +0800

    创建通知解析规则相关界面

---
+++ app/src/main/java/com/project/manager/ui/setting/auto_bookkeeping/NotificationAnalysisActivity.java
@@ -0,0 +1,16 @@
+package com.project.manager.ui.setting.auto_bookkeeping;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.project.manager.R;
+
+public class NotificationAnalysisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_analysis);
+    }
+}
