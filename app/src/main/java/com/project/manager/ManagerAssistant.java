2b96f144a0050d84b43ab4b74c47beb14d9b6b3d
E_zhiyu <E82487318@outlook.com>
2026-01-14 11:13:37 +0800

    成功删除安装包会发出提示

--- app/src/main/java/com/project/manager/ManagerAssistant.java
+++ app/src/main/java/com/project/manager/ManagerAssistant.java
@@ -3,6 +3,7 @@ package com.project.manager;
 import android.app.Application;
 import android.net.Uri;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
@@ -12,6 +13,7 @@ import com.google.android.material.color.DynamicColorsOptions;
 import com.project.manager.data.data_save.preference.AutoBackupPreference;
 import com.project.manager.data.data_save.preference.AppSettingsPreference;
 import com.project.manager.data.data_save.preference.VersionPreference;
+import com.project.manager.enums.LogTags;
 import com.project.manager.helpers.AutoBackupHelper;
 import com.project.manager.workers.BackupScheduler;
 
@@ -57,6 +59,7 @@ public class ManagerAssistant extends Application {
             File apkFile = new File(Objects.requireNonNull(Uri.parse(apkUri).getPath()));
             if (apkFile.exists() && apkFile.delete()) {
                 Log.d(LogTags.APPLICATION.getV(), String.format(Locale.getDefault(), "成功删除“%s”", apkFile.getName()));
+                Toast.makeText(this, "已自动删除安装包", Toast.LENGTH_SHORT).show();
             } else {
                 Log.w(LogTags.APPLICATION.getV(), String.format(Locale.getDefault(), "“%s”删除失败", apkFile.getName()));
             }
