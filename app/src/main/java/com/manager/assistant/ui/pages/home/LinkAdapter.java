a82ff6f51c71085ca795a0ddf0e4e0e498cd88d2
E_zhiyu <E82487318@outlook.com>
2026-01-25 09:47:21 +0800

    修改主页链接文本样式

--- app/src/main/java/com/manager/assistant/ui/pages/home/LinkAdapter.java
+++ app/src/main/java/com/manager/assistant/ui/pages/home/LinkAdapter.java
@@ -35,6 +35,7 @@ public class LinkAdapter extends RecyclerView.Adapter<LinkAdapter.LinkViewHolder
         this.context = context;
     }
 
+    @SuppressLint("ResourceType")
     @NonNull
     @Override
     public LinkViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -46,6 +47,7 @@ public class LinkAdapter extends RecyclerView.Adapter<LinkAdapter.LinkViewHolder
         ));
         textView.setPadding(0, 5, 16, 5);
         textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);   //设置文字大小为17sp
+        textView.setTextAppearance(context, com.google.android.material.R.attr.textAppearanceBodyMedium);
 
         return new LinkViewHolder(textView);
     }
