83a32dbb5f592270154b4cacb57b67a54cc2a75f
E_zhiyu <E82487318@outlook.com>
2026-01-30 17:06:03 +0800

    修复流水记录过滤器弹窗弹出时崩溃的BUG

--- app/src/main/java/com/manager/assistant/data/data_class/Tag.java
+++ app/src/main/java/com/manager/assistant/data/data_class/Tag.java
@@ -159,7 +159,8 @@ public class Tag {
 
         String[] columns = {
                 BookKeepingColumns.TAG_NAME.toString(),
-                BookKeepingColumns.TAG_NO.toString()
+                BookKeepingColumns.TAG_NO.toString(),
+                BookKeepingColumns.TAG_SCOPE.toString()
         };
         Cursor cursor = db.query(
                 BookKeepingTables.TAG.toString(),
