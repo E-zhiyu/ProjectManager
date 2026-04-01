3c4b85997f3d2f937a3bc6d5f0cb8220c75d406e
E_zhiyu <E82487318@outlook.com>
2026-04-01 14:20:29 +0800

    更新依赖；修改版本号；修改更新日志

--- app/src/main/java/com/manager/assistant/helpers/about/UpdateLogHelper.java
+++ app/src/main/java/com/manager/assistant/helpers/about/UpdateLogHelper.java
@@ -11,7 +11,10 @@ import com.manager.assistant.R;
 import io.noties.markwon.Markwon;
 
 public class UpdateLogHelper {
-    private static final String UPDATE_LOG_MD = "# v1.8.10  \n" +
+    private static final String UPDATE_LOG_MD = "# v1.8.11  \n" +
+            "**修改内容**  \n" +
+            "- 使用全面屏手势时所有界面支持沉浸式小白条  \n\n" +
+            "# v1.8.10  \n" +
             "**BUG修复**  \n" +
             "- 修复标签选择文本框会出现多余的文本清除按钮的BUG  \n" +
             "- 修复第一次启动可能直接崩溃的BUG  \n\n" +
