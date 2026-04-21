8a1880fa7b0fd972580d042f124e3dded76ed06a
E_zhiyu <E82487318@outlook.com>
2026-04-21 20:21:01 +0800

    修改更新日志

--- app/src/main/java/com/manager/assistant/helpers/about/UpdateLogHelper.java
+++ app/src/main/java/com/manager/assistant/helpers/about/UpdateLogHelper.java
@@ -14,6 +14,7 @@ public class UpdateLogHelper {
     private static final String UPDATE_LOG_MD = "# v1.8.15  \n" +
             "### 修改和优化的内容  \n" +
             "- 优化增大卡片圆角后报表界面内容的显示效果  \n" +
+            "- 删除依赖：Groupie，用ConcatAdapter替代其实现流水记录列表  \n" +
             "- 标签分组中标签数量为0时不再能折叠分组  \n" +
             "# v1.8.14  \n" +
             "### BUG修复  \n" +
