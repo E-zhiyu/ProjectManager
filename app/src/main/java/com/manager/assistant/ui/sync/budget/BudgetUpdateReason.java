7032feab9b0e1bbfde7fc738a5ed6f82ad5ef69a
E_zhiyu <E82487318@outlook.com>
2026-07-22 15:11:55 +0800

    删除无用的类

--- app/src/main/java/com/manager/assistant/ui/sync/budget/BudgetUpdateReason.java
+++
@@ -1,9 +0,0 @@
-package com.manager.assistant.ui.sync.budget;
-
-public enum BudgetUpdateReason {
-    ADD,        //添加
-    MODIFIED,   //修改
-    DELETED,    //删除
-    CLEARED,    //清除
-    REFRESHED   //刷新
-}
