40d2f1fef5111d7f25965a3cf5dc33b83654ab3d
E_zhiyu <E82487318@outlook.com>
2026-07-23 14:30:32 +0800

    清理代码

--- app/src/main/java/com/manager/assistant/data/controllers/TagDataController.java
+++ app/src/main/java/com/manager/assistant/data/controllers/TagDataController.java
@@ -5,14 +5,9 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 
-import androidx.annotation.NonNull;
-
-import com.manager.assistant.data.classes.Tag;
 import com.manager.assistant.data.save.database.BookkeepingDbHelper;
 import com.manager.assistant.data.save.database.Columns;
 import com.manager.assistant.data.save.database.Tables;
-
-import org.jetbrains.annotations.Contract;
 
 public class TagDataController {
 
@@ -52,72 +47,6 @@ public class TagDataController {
         cursor.close();
         db.close();
         return tagName;
-    }
-
-    /**
-     * 获取通知解析规则的标签实例
-     *
-     * @param ruleNo  规则编号
-     * @param context 上下文
-     * @return 获取到的标签实例
-     * @throws SQLiteException 读取数据库可能引发的异常
-     */
-    @NonNull
-    @Contract("_, _ -> new")
-    public static Tag getTagByRuleNo(long ruleNo, Context context) throws SQLiteException {
-        BookkeepingDbHelper dbHelper = new BookkeepingDbHelper(context);
-        SQLiteDatabase db = dbHelper.openWriteLink();
-
-        //查询标签编号
-        String[] columns = {Columns.TAG_NO.toString()};
-        String selection = Columns.RULE_NO + "=?";
-        String[] selectionArgs = {String.valueOf(ruleNo)};
-        Cursor ruleCursor = db.query(
-                Tables.ANALYSIS_RULE.toString(),
-                columns,
-                selection,
-                selectionArgs,
-                null,
-                null,
-                null,
-                "1"
-        );
-
-        long tagNo = 0;
-        if (ruleCursor.moveToNext()) {
-            tagNo = ruleCursor.getLong(ruleCursor.getColumnIndexOrThrow(Columns.TAG_NO.toString()));
-            if (tagNo == 0)
-                return new Tag("", 0, 0);
-        }
-
-        //查询标签名称
-        String[] tagColumns = {
-                Columns.TAG_NAME.toString(),
-                Columns.TAG_SCOPE.toString()
-        };
-        String tagSelection = Columns.TAG_NO + "=?";
-        String[] tagSelectionArgs = {String.valueOf(tagNo)};
-        Cursor tagCursor = db.query(
-                Tables.TAG.toString(),
-                tagColumns,
-                tagSelection, tagSelectionArgs,
-                null,
-                null,
-                null,
-                "1"
-        );
-
-        String tagName = "";
-        int scope = 0;
-        if (tagCursor.moveToNext()) {
-            tagName = tagCursor.getString(tagCursor.getColumnIndexOrThrow(Columns.TAG_NAME.toString()));
-            scope = tagCursor.getInt(tagCursor.getColumnIndexOrThrow(Columns.TAG_SCOPE.toString()));
-        }
-
-        ruleCursor.close();
-        tagCursor.close();
-        db.close();
-        return new Tag(tagName, tagNo, scope);
     }
 
 }
