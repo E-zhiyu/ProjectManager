aa9767b438364e4d02de62a236e49e1fdc4eeb0e
E_zhiyu <E82487318@outlook.com>
2026-08-02 14:42:46 +0800

    自动记账新增直接入账功能

--- app/src/main/java/com/sly/coffer/data/save/preference/AutoBookKeepingPreference.java
+++ app/src/main/java/com/sly/coffer/data/save/preference/AutoBookKeepingPreference.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 public class AutoBookKeepingPreference {
     private static final String PREF_NAME = "AutoBookKeepingPreference";
     private static final String KEY_NOTIFICATION_ANALYSIS_OPENED = "notification_analysis_opened";
+    private static final String KEY_DIRECT_DEPOSIT = "direct_deposit";              //直接入账开关
     private static final String KEY_NOTIFICATION_CANCEL = "notification_cancel";    //自动记账通知点击行为
     private static final String KEY_NOTIFICATION_CLICK = "notification_click";      //自动记账确认通知点击行为
 
@@ -22,6 +23,28 @@ public class AutoBookKeepingPreference {
     public static boolean getSwitchStat(@NonNull Context context) {
         SharedPreferences pref = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
         return pref.getBoolean(KEY_NOTIFICATION_ANALYSIS_OPENED, false);
+    }
+
+    /**
+     * 设置直接入帐开关状态
+     *
+     * @param isOpened 开关是否开启
+     * @param context  上下文
+     */
+    public static void setDirectDeposit(boolean isOpened, @NonNull Context context) {
+        SharedPreferences pref = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        pref.edit().putBoolean(KEY_DIRECT_DEPOSIT, isOpened).apply();
+    }
+
+    /**
+     * 获取直接入帐开关状态
+     *
+     * @param context 上下文
+     * @return 直接入帐功能是否打开
+     */
+    public static boolean getDirectDeposit(@NonNull Context context) {
+        SharedPreferences pref = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        return pref.getBoolean(KEY_DIRECT_DEPOSIT, false);
     }
 
     /**
