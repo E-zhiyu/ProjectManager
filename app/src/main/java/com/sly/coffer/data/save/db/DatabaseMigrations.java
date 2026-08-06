b7646435e605a03e65995145a0495a7ea0690119
E_zhiyu <E82487318@outlook.com>
2026-08-06 12:08:41 +0800

    修复数据库更新失败的BUG

--- app/src/main/java/com/sly/coffer/data/save/db/DatabaseMigrations.java
+++ app/src/main/java/com/sly/coffer/data/save/db/DatabaseMigrations.java
@@ -8,7 +8,7 @@ public class DatabaseMigrations {
     static final Migration MIGRATION_1_2 = new Migration(1, 2) {
         @Override
         public void migrate(@NonNull SupportSQLiteDatabase db) {
-            db.execSQL("CREATE TABLE IF NOT EXISTS `capturedNotifications` (`notificationId` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `title` TEXT, `content` TEXT, `packageName` TEXT, `appName` TEXT, `time` INTEGER NOT NULL)");
+            db.execSQL("CREATE TABLE IF NOT EXISTS `capturedNotifications` (`notificationId` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `title` TEXT, `content` TEXT, `packageName` TEXT, `appName` TEXT, `time` INTEGER)");
             db.execSQL("CREATE INDEX IF NOT EXISTS `index_capturedNotifications_title` ON `capturedNotifications` (`title`)");
             db.execSQL("CREATE INDEX IF NOT EXISTS `index_capturedNotifications_content` ON `capturedNotifications` (`content`)");
             db.execSQL("CREATE INDEX IF NOT EXISTS `index_capturedNotifications_appName` ON `capturedNotifications` (`appName`)");
